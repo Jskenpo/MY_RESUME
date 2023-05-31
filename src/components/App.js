ab2abd5d7f6c109047d059357f2aa20360cd1885
jsantis <san21153@uvg.edu.gt>
2023-05-30 19:53:03 -0600

    configuración de proyecto

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>React App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
